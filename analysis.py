dc8a69d6b8bf6c43c5a35f4eff7db08c3572a15d
Thiago Lima <thenriq@gmail.com>
2020-04-18 21:06:40 +0100

    added histograms and scatterplot + print_to_file

--- analysis.py
+++ analysis.py
@@ -1,8 +1,13 @@
 # Load libraries
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
-from matplotlib import pyplot
-
+from matplotlib import pyplot as plt
+from sklearn import datasets 
 # Load Dataset
 
 filename = open("iris.data","r")
@@ -15,19 +20,62 @@ dataset = read_csv(filename,names=names)
 print(dataset.head(20))
 
 # descriptions
-print(dataset.describe())
+with open("dataset.txt","a") as f:
+    print((dataset.describe()),file = f)
 
 # class distribution
 print(dataset.groupby('class').size())
 
+
 # box and whisker plots
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-pyplot.show()
+plt.show()
+
+
 
 # histograms
-dataset.hist()
-pyplot.show()
+# Sepal Length
+dataset.hist(column='sepal-length',rwidth=0.9)
+plt.title('Sepal Length')
+plt.xlabel('Length (cm)')
+plt.ylabel('Quantity')
+plt.savefig('Sepal_Length.png')
+#py.clf()
 
-# scatter plot matrix
-scatter_matrix(dataset)
-pyplot.show()
+# Sepal Width
+dataset.hist(column='sepal-width',rwidth=0.9)
+plt.title('Sepal Width')
+plt.xlabel('Length (cm)')
+plt.ylabel('Quantity')
+plt.savefig('Sepal_Width.png')
+#py.clf()
+
+# Petal Length
+dataset.hist(column='petal-length',rwidth=0.9)
+plt.title('Petal Length')
+plt.xlabel('Length (cm)')
+plt.ylabel('Quantity')
+plt.savefig('Petal_Length.png')
+#plt.clf()
+
+# Petal Width
+dataset.hist(column='petal-width',rwidth=0.9)
+plt.title('Petal Width')
+plt.xlabel('Length (cm)')
+plt.ylabel('Quantity')
+plt.savefig('Petal_Width.png')
+
+
+plt.close("all") # closing all previous open plots
+
+
+# Scatterplot
+sns.scatterplot(x="sepal-length", y="sepal-width", data=dataset,  hue="class")
+plt.title("Sepal")
+plt.figure()
+
+
+sns.scatterplot(x="petal-length", y="petal-width", data=dataset,  hue="class")
+plt.title("Petal")
+plt.show()
+
